3401a13874f94b0e594aa1de1ba682087a7f07c6
unknown <agarcia@LIC-10.GOBELINS-ANNECY.local>
2016-01-08 14:54:05 +0100

    first commit

---
+++ Assets/Assets/Scripts/FireBow.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBow : MonoBehaviour {
+
+	public Rigidbody arrowPrefab;
+
+	private Vector3 distance;
+	private Vector3 minDistance = new Vector3(5.5f, 0, 5.5f);
+	private Vector3 defaultPos;
+	private Vector3 heroPos;
+
+	private float speed = 90;
+	private float step;
+	
+
+	Animator animator;
+
+	Transform heroT;
+	Transform arrowT;
+
+	GameObject hero;
+	GameObject arrow;
+	
+	Rigidbody arrowRb;
+	Rigidbody arrowCloneRb;
+
+	private bool firingNow;
+
+
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator>();
+
+		hero = GameObject.Find ("ThirdPersonController");
+		arrow = transform.Find ("Arrow").gameObject;
+		
+		heroT = hero.transform;
+		arrowT = arrow.transform;
+	
+		animator.SetBool ("HasFinished", true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		distance = transform.position - heroT.position;
+			
+		if (((Mathf.Abs (distance.x) < minDistance.x) && (Mathf.Abs (distance.z) < minDistance.z)) && animator.GetBool("HasFinished")) {
+			animator.SetBool ("IsFiring", true);
+		}		
+	}
+
+
+	public void FireArrow()
+	{
+
+		Rigidbody arrowClone = (Rigidbody) Instantiate(arrowPrefab);
+
+		arrowClone.transform.LookAt(heroT);
+
+		arrowClone.AddForce(arrowT.forward * 500);
+
+		Destroy(arrowClone, 1);
+
+	}
+
+
+
+	public void FiringFalse() {
+		animator.SetBool ("HasFinished", true);
+		animator.SetBool ("IsFiring", false);
+	}
+	public void FiringTrue() {
+		animator.SetBool ("HasFinished", false);
+	}
+
+
+	
+}
---
+++ Assets/PotionController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotionController : MonoBehaviour {
+
+	private float amplitude;          //Set in Inspector 
+	private float speed;                  //Set in Inspector 
+	private float tempVal;
+	private Vector3 tempPos;
+
+	// Use this for initialization
+	void Start () {
+		amplitude = 0.15f;
+		speed = 0.7f;
+		tempVal = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		tempPos.y = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+		tempPos.x = transform.position.x;
+		tempPos.z = transform.position.z;
+	
+		transform.position = tempPos;
+	}
+}
+
+
